3c27feda85be3706f4b46ca6920c36be0841bcb8
Pranay Gaikwad <pranaygaikwad407@gmail.com>
2020-10-26 19:45:37 +0530

    functional component

---
+++ src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+//using noraml javascript function
+
+// function Greet () {
+//     return <h1>Hello Pranay</h1>
+// }
+
+//using arrow function
+
+const Greet = () => {
+    return <h1>Hello </h1>
+}
+
+
+export default Greet;
